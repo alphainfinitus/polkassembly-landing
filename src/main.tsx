6b7d02630e146cb2c20646b0153e2fb97cb023c1
Mridul Kumar <kumar.mridul000@gmail.com>
2022-08-11 17:16:37 +0530

    removes baseName

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename='/polkassembly.io'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
