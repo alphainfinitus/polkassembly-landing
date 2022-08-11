be9e76d5afa1c176f580b9927c74cfa5ef216d77
Mridul Kumar <kumar.mridul000@gmail.com>
2022-08-11 15:27:55 +0530

    Pagination arrow opacity on hover

--- src/components/Home/LiveChainsSection.tsx
+++ src/components/Home/LiveChainsSection.tsx
@@ -63,7 +63,7 @@ export default function LiveChainsSection() {
 
     return (
       <div className='flex items-center'>
-        <button onClick={() => {if(currPage != 1) setCurrPage(currPage - 1)}} className='mx-4 text-pa-purple hover:text-pa-pink'>
+        <button onClick={() => {if(currPage != 1) setCurrPage(currPage - 1)}} className='mx-4 text-pa-pink/50 hover:text-pa-pink'>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
           </svg>
@@ -71,7 +71,7 @@ export default function LiveChainsSection() {
 
         {divItems}
 
-        <button onClick={() => {if(currPage != totalPages) setCurrPage(currPage + 1)}} className='mx-4 text-pa-purple hover:text-pa-pink'>
+        <button onClick={() => {if(currPage != totalPages) setCurrPage(currPage + 1)}} className='mx-4 text-pa-pink/50 hover:text-pa-pink'>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
           </svg>
