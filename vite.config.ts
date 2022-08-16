dd8c66e48a869a58dd499e3325e699671d760fa2
Mridul Kumar <kumar.mridul000@gmail.com>
2022-08-16 16:07:37 +0530

    built without base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/polkassembly-landing/',
+  base: '/polkassembly-landing/', //TODO: REMOVE FOR PA BUILD
   plugins: [react()]
 })
